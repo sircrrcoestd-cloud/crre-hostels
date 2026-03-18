87d8e5e989f5c2a14910e57d47b385800754dcbe
TarunB8 <saitarun2005p@gmail.com>
2026-03-18 16:00:32 +0530

    changed mysql2

--- server.js
+++ server.js
@@ -1,7 +1,6 @@
 const express = require("express");
 
 const bodyParser = require("body-parser");
-const mysql = require("mysql2/promise");
 const path = require("path");
 const app = express();
 const crypto = require("crypto");
